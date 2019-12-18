6cbc935c7e5a12f2438c7ed0fca2660fbe76cb58
Hardikkumar.Patel <hardikkumar.ce@gmail.com>
2019-12-17 18:56:31 -0600

    Adding intial files

--- app/containers/ConnectionPage.js
+++ app/containers/ConnectionPage.js
@@ -1,10 +1,29 @@
 // @flow
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import ConnectionListPage from '../components/connections/ConnectionListPage'
-
+import { fetchConnections } from '../store/connections/actionCreator'
 // $FlowFixMe
-export default class ConnectionPage extends Component<Props> {
+class ConnectionPage extends Component<Props> {
   render() {
-    return <ConnectionListPage />
+    const { connections } = this.props
+    return <ConnectionListPage connections={connections} />
   }
 }
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      fetchConnections,
+    },
+    dispatch
+  )
+const mapStateToProps = state => {
+  const { connections } = state
+  return {
+    connections,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConnectionPage)
